331b5f9905592c331fc7004e844dee8157ebea4a
Varvara Lapteva <javarvara82@gmail.com>
2024-10-08 14:41:49 +0300

    editing 2_4_8.py

--- 2_4_8.py
+++ 2_4_8.py
@@ -1,11 +1,12 @@
-from selenium import webdriver
+#Задание: ждем нужный текст на странице 2.4.8
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-import time, math
+import time
+import math
 
 def calc(x):
   return str(math.log(abs(12*math.sin(int(x)))))
@@ -13,17 +14,12 @@ def calc(x):
 browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 browser.get("http://suninjuly.github.io/explicit_wait2.html")
 
-
 WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
 button1 = browser.find_element(By.CSS_SELECTOR, "[onclick='checkPrice();']")
 button1.click()
 
-
 x_element = browser.find_element(By.CSS_SELECTOR, "[id='input_value']")
 x = calc(x_element.text)
-
-print(x_element.text)
-print(x_element.get_attribute())
 
 input1 = browser.find_element(By.ID, "answer")
 browser.execute_script("return arguments[0].scrollIntoView(true);", input1)
